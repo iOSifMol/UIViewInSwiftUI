9594f5b01151f484ed4f0a8e0a9fc15358c9f4f1
TAGonSoft <iosif.moldovan@tagonsoft.ro>
2022-02-07 20:42:25 +0200

    init

---
+++ UIViewInSwiftUI/UIViewInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewInSwiftUIApp.swift
+//  UIViewInSwiftUI
+//
+//  Created by Iosif Moldovan on 07.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct UIViewInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
